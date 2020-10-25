af2d10af4dab9d35122b584d344faa9b670144b0
linusmbae <linusmutugi5178@gmail.com>
2020-10-25 06:55:41 +0300

    today's updates

--- app/src/main/java/org/wifi_manager/ui/MapActivity.java
+++ app/src/main/java/org/wifi_manager/ui/MapActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.SearchView;
 import android.widget.Toast;
 
@@ -16,18 +18,26 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.trncic.library.DottedProgressBar;
 
 import org.wifi_manager.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
     GoogleMap map;
+    @BindView(R.id.maps_progress) DottedProgressBar progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        ButterKnife.bind(this);
+
+        startTimer();
 
         try {
             SupportMapFragment mapFragment=(SupportMapFragment) getSupportFragmentManager()
@@ -43,6 +53,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         try {
+            stopProgress();
+            progress.setVisibility(View.INVISIBLE);
             map=googleMap;
             LatLng home = new LatLng(-1.2840425907023074, 36.70896668144134);
             map.addMarker(new MarkerOptions().position(home).title("Home"));
@@ -51,6 +63,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             new SweetAlertDialog(MapActivity.this,SweetAlertDialog.ERROR_TYPE)
                     .setTitleText("Errors")
                     .show();
+            stopProgress();
+            progress.setVisibility(View.INVISIBLE);
         }
     }
 
@@ -84,5 +98,25 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void startTimer() {
+        Runnable run = new Runnable(){
+
+            @Override
+            public void run() {
+                startProgress();
+            }
+        };
+        Handler han = new Handler();
+        han.postAtTime(run, 100);
+    }
+
+    public void stopProgress() {
+        progress.stopProgress();
+    }
+
+    public void startProgress() {
+        progress.startProgress();
     }
 }
